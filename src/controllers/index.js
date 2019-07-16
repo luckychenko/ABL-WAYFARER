e32d959536307619e6cae795b071587b9c26553b
Lucky Ogidan <luckychenko@gmail.com>
2019-07-16 20:16:24 +0100

    initial commit

--- src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-
-import auth from './auth';
-import secured from './api';
-
-module.exports = {
-  auth,
-  secured,
-};
--- src/models/user.js
+++
@@ -1,6 +0,0 @@
-pool.query('SELECT * FROM public."User"', (error, results) => {
-  if (error) {
-    throw error;
-  }
-  res.status(200).json(results.rows);
-});
